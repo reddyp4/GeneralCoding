07bd002c34e8debab61042e996e208cae0c1417a
madison <reddynisky145@gmail.com>
2022-03-05 16:43:46 -0800

    update

---
+++ Python/031_Turtle_PyPi.py
@@ -0,0 +1,15 @@
+#from turtle import Screen, Turtle
+#newTurtleObject = Turtle()
+#print(newTurtleObject)
+#Change shape to turtle
+#newTurtleObject.shape("turtle")
+#Change color, fill=green, pen=red
+#newTurtleObject.color("red", "green")
+#newScreen = Screen()
+#print(newScreen.canvheight)
+#move by 100 spaces
+#newTurtleObject.forward(100)
+#newScreen.exitonclick()
+
+import prettytable
+
