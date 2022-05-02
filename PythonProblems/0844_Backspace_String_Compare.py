4e4cd51f771c7f89fe6c0e715deea81acbc217a5
PatelR <reddynisky145@gmail.com>
2022-05-02 07:41:18 -0700

    Problem 844

--- PythonProblems/0844_Backspace_String_Compare.py
+++ PythonProblems/0844_Backspace_String_Compare.py
@@ -23,34 +23,31 @@ Explanation: s becomes "c" while t becomes "b".
 #    def backspaceCompare(self, s: str, t: str) -> bool:
 
 #Method-1: Brute Force - evaluate strings, string compare
-def backspaceString(s: str) -> str:
-    len1=len(s)
-    print(f"Length of s={len1}")
-    s1 = ""
-    for i in range(0,len1-2):
-        print(s[i])
-        if(s[i+1]=="#"):
-            i+=1
-        else:
-            s1=s1+s[i]
-    if(s[len1-1]!="#"):
-        s1=s1+s[len1-1]
-    return s1
+class Solution:
+    def backspaceString(self, s: str) -> str:
+        len1=len(s)
+        s1 = ""
+        for i in range(0,len1-2):
+            if(s[i+1]=="#"):
+                i+=1
+            else:
+                s1=s1+s[i]
+        if(s[len1-1]!="#"):
+            s1=s1+s[len1-1]
+        return s1
 
-def backspaceCompare(s: str, t: str) -> bool:
-    s1=backspaceString(s)
-    #print(f"New Strings:{s1}")
-    t1=backspaceString(t)
-    print(f"New Strings:{s1},{t1}")
-    if(s1==t1):
-        return True
-    else:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s1=self.backspaceString(s)
+        t1=self.backspaceString(t)
+        if(s1==t1):
+            return True
+        else:
+            return False
         return False
-    return False
 
 s="ab#c"
 t="ad#c"
-val=backspaceCompare(s,t)
+val=Solution.backspaceCompare(s,t)
 if(val):
     print(f"s:{s},t={t} are same!")
 else:
