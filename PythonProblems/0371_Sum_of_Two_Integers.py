a6d4d61074279009555f38e368baa3113a6518f1
PatelR <reddynisky145@gmail.com>
2022-05-03 08:27:04 -0700

    Start371

---
+++ PythonProblems/0371_Sum_of_Two_Integers.py
@@ -0,0 +1,30 @@
+''''
+371. Sum of Two Integers
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = 2, b = 3
+Output: 5
+ 
+
+Constraints:
+
+-1000 <= a, b <= 1000
+'''
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        #Get binary representations
+        #Get xor to see non-same bits
+        #Shift bit for zero xor results
+        return sum
+
+a=1
+b=2
+sol=Solution()
+print(f"Sum={sol.getSum(a,b)}")
