bd11d4a96993b4c5e021b110ca9f5cd7e434c3d7
PatelR <reddynisky145@gmail.com>
2022-06-16 12:37:18 -0700

    Permutations! Yay!

--- PythonProblems/CoursePrep/Recursion/046_Permutations.py
+++ PythonProblems/CoursePrep/Recursion/046_Permutations.py
@@ -23,7 +23,61 @@ Constraints:
 -10 <= nums[i] <= 10
 All the integers of nums are unique.
 '''
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        
 
+# Brute-force Method1: Recursive, each worker picks element at i, pops it in reduceList 
+# and adds to partial list
+# Call recursive subproblem for each i from i+1 to end
+# Base problem1: length(nums)==1, since we are popping 
+# Can we back-track once len(nums)==1
+
+from typing import List
+
+class Solution:
+    debug = 0
+
+    def subProblem(self, nums: List[int], partial: List[int], final: List[int]):
+        #Run base case
+        if(len(nums)==1):
+            a = nums.pop()
+            partial.append(a)
+            final.append(list(partial))
+            if(self.debug==1):
+                print(f"Base: partial:{partial},nums:{nums},final={final}")
+            a = partial.pop()
+            nums.append(a)
+            if(self.debug==1):
+                print(f"Base Return:partial:{partial},nums:{nums},final={final}")
+            return
+        #Include element i, push index to Hash table, pop from remaining
+        # Call worker for each i, pop first element in nums, add to partial, 
+        # call subProblem on nums
+        # once returned pop the element to move to next ith element
+        for index in range(0,len(nums)):
+            #pop element out of nums
+            a = nums.pop(index)
+            #push element into partial
+            partial.append(a)
+            if(self.debug==1):
+                print(f"index:{index},partial:{partial},nums:{nums},final={final}")
+            #call next level
+            self.subProblem(nums,partial,final)
+            #do I need to pop from partial and enter back into nums at index
+            if(self.debug==1):
+                print(f"returned index:{index},partial:{partial},nums:{nums},final={final}")
+            a=partial.pop()
+            nums.insert(index,a)
+            if(self.debug==1):
+                print(f"finished index:{index},partial:{partial},nums:{nums},final={final}")
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        final = []
+        newList = nums
+        partial = []
+        self.subProblem(newList, partial, final)
+        return final
+
+nums = [1,2,3]
+print(f"Initial list:{nums}")
+sol = Solution()
+permut = sol.permute(nums)
+print(f"Final list:{permut}")
