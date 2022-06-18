6bca97f22a3c64d59db79838342fb63d5dcc13e1
PatelR <reddynisky145@gmail.com>
2022-06-18 07:56:52 -0700

    Changes

---
+++ PythonProblems/LCSorting/0001_Two_Sum.py
@@ -0,0 +1,69 @@
+''''
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+'''
+
+# Method1: Sort, and use two pointer approach
+# Problem if sorted loosing the index
+# Method1: Brute-force
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        final=[]
+        for left in range(0,len(nums)):
+            for right in range(left+1,len(nums)):
+                if((nums[left]+nums[right])==target):
+                    #Found solution
+                    final.append(left)
+                    final.append(right)
+                    print(f"left={left},right={right},nums[left]={nums[left]},nums[right]={nums[right]}")
+                    return final
+        return final        
+
+nums = [2,7,11,15]
+target = 9
+
+#nums = [3,3]
+#target = 6
+
+#nums = [3,2,4]
+#target = 6
+
+sol=Solution()
+final=sol.twoSum(nums,target)
+print(f"Solution:nums={nums},target={target},final={final}")
