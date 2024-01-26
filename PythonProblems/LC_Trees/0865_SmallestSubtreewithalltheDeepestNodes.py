c0c1425153e45a388e76989b88677c4792d7e1e7
reddyp4 <bhageerath.reddy@gmail.com>
2024-01-25 21:02:40 -0800

    865. Smallest Subtree with all the Deepest Nodes

---
+++ PythonProblems/LC_Trees/0865_SmallestSubtreewithalltheDeepestNodes.py
@@ -0,0 +1,105 @@
+'''
+865. Smallest Subtree with all the Deepest Nodes
+Solved
+Medium
+Topics
+Companies
+Given the root of a binary tree, the depth of each node is the shortest distance to the root.
+
+Return the smallest subtree such that it contains all the deepest nodes in the original tree.
+
+A node is called the deepest if it has the largest depth possible among any node in the entire tree.
+
+The subtree of a node is a tree consisting of that node, plus the set of all descendants of that node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4]
+Output: [2,7,4]
+Explanation: We return the node with value 2, colored in yellow in the diagram.
+The nodes coloured in blue are the deepest nodes of the tree.
+Notice that nodes 5, 3 and 2 contain the deepest nodes in the tree but node 2 is the smallest subtree among them, so we return it.
+Example 2:
+
+Input: root = [1]
+Output: [1]
+Explanation: The root is the deepest node in the tree.
+Example 3:
+
+Input: root = [0,1,3,null,2]
+Output: [2]
+Explanation: The deepest node in the tree is 2, the valid subtrees are the subtrees of nodes 2, 1 and 0 but the subtree of node 2 is the smallest.
+ 
+
+Constraints:
+
+The number of nodes in the tree will be in the range [1, 500].
+0 <= Node.val <= 500
+The values of the nodes in the tree are unique.
+ 
+
+Note: This question is the same as 1123: https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    LCA = TreeNode(-1)
+    found = 0
+
+    #Function to find LCA of deepest nodes: Nodes
+    def getLCA(self,root,Nodes):
+        count,left,right=0,0,0
+        for index in range(len(Nodes)):
+            if(Nodes[index]==root):
+                count=1
+                break
+        if(root.left):
+            left=self.getLCA(root.left,Nodes)
+        if(root.right):
+            right=self.getLCA(root.right,Nodes)
+        if(count+left+right>=len(Nodes)):
+            if(self.found==0):
+                self.LCA=root
+                self.found=1
+        return (left+right+count)
+    
+    #Function to make list of deepest nodes
+    def getDeepest(self,root,count,maxcount,Nodes):
+        leftmaxcount,rightmaxcount=0,0
+        if(root.left is None and root.right is None):
+            if(count+1==maxcount):
+                #newleaf as previous
+                Nodes.append(root)
+            elif(count+1>maxcount):
+                #print("root:",root.val,"count:",count,"maxcount:",maxcount)
+                #newleaf
+                maxcount=count+1
+                while(len(Nodes)!=0):
+                    Nodes.pop()
+                Nodes.append(root)
+            return maxcount
+        if(root.left):
+            leftmaxcount=self.getDeepest(root.left,count+1,maxcount,Nodes)
+        maxcount = leftmaxcount if (leftmaxcount>maxcount) else maxcount
+        if(root.right):
+            rightmaxcount=self.getDeepest(root.right,count+1,maxcount,Nodes)
+        maxcount = rightmaxcount if (rightmaxcount>maxcount) else maxcount
+        return maxcount
+        
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        Nodes=[]
+        maxDepth=self.getDeepest(root,0,0,Nodes)
+        #print("maxDepth:",maxDepth)
+        #print(Nodes)
+        self.getLCA(root,Nodes)
+        #print("LCA:",self.LCA)
+        #Get BFS of LCA
+        return self.LCA
