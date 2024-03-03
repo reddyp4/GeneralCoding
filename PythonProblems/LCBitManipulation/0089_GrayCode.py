8f76acc4537c4b011bfab391956892516a0b8b78
reddyp4 <bhageerath.reddy@gmail.com>
2024-03-02 17:41:51 -0800

    GrayCode LC

---
+++ PythonProblems/LCBitManipulation/0089_GrayCode.py
@@ -0,0 +1,58 @@
+'''
+89. Gray Code
+Solved
+Medium
+Topics
+Companies
+An n-bit gray code sequence is a sequence of 2n integers where:
+
+Every integer is in the inclusive range [0, 2n - 1],
+The first integer is 0,
+An integer appears no more than once in the sequence,
+The binary representation of every pair of adjacent integers differs by exactly one bit, and
+The binary representation of the first and last integers differs by exactly one bit.
+Given an integer n, return any valid n-bit gray code sequence.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,3,2]
+Explanation:
+The binary representation of [0,1,3,2] is [00,01,11,10].
+- 00 and 01 differ by one bit
+- 01 and 11 differ by one bit
+- 11 and 10 differ by one bit
+- 10 and 00 differ by one bit
+[0,2,3,1] is also a valid gray code sequence, whose binary representation is [00,10,11,01].
+- 00 and 10 differ by one bit
+- 10 and 11 differ by one bit
+- 11 and 01 differ by one bit
+- 01 and 00 differ by one bit
+Example 2:
+
+Input: n = 1
+Output: [0,1]
+ 
+
+Constraints:
+
+1 <= n <= 16
+'''
+class Solution:
+    def binary2gray(self, n):
+        return n^(n>>1)
+
+    def grayCode(self, n: int) -> List[int]:
+        #Following Wiki article
+        #https://en.wikipedia.org/wiki/Gray_code#Converting_to_and_from_Gray_code
+        result=[]
+        for i in range(2**n):
+            result.append(self.binary2gray(i))
+            #result.append((i^(i>>1)))
+        return result
+
+'''
+Follow this article: https://en.wikipedia.org/wiki/Gray_code#Converting_to_and_from_Gray_code
+'''
