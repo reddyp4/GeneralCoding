1f974c054b7384d08f5f97c53a4e826b87a78d2b
reddyp4 <bhageerath.reddy@gmail.com>
2024-10-02 14:28:01 -0700

    computerSquare

---
+++ Embedded/Problems/EmbeddedProblemsProgress/Debug_computerSquareADC.C
@@ -0,0 +1,6 @@
+//    The function computeSquareADC() has not been producing correct
+//    output consistently. Please describe all issues with the function.
+
+1) Overflow
+2) Truncating
+3) ADC values are volatile, so result may be outdated
