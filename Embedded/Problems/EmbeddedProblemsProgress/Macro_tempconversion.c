aa4ec288e67882da488dc9b12fae8c29a5fd19c5
reddyp4 <bhageerath.reddy@gmail.com>
2024-10-02 14:23:22 -0700

    macro temp conversion

---
+++ Embedded/Problems/EmbeddedProblemsProgress/Macro_tempconversion.c
@@ -0,0 +1,4 @@
+//    Create a macro (named C_TO_F) to convert from degrees Celsius to Fahrenheit
+//    Macro should work for integer or floating point types
+
+#DEFINE C_TO_F(degC)  (degC*(9.0f/5.0f)+32.0f)
