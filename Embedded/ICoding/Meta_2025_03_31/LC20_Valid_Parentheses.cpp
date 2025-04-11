cfe3fa5b528f89b68304dda060b7268dd4ba30e6
reddyp4 <bhageerath.reddy@gmail.com>
2025-04-11 06:20:04 -0700

    paranthesis

---
+++ Embedded/ICoding/Meta_2025_03_31/LC20_Valid_Parentheses.cpp
@@ -0,0 +1,72 @@
+/*
+20. Valid Parentheses
+Solved
+Easy
+Topics
+Companies
+Hint
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+*/
+
+class Solution {
+    public:
+        bool isValid(string s) {
+            stack<char> par;
+            int valid=1,lens=s.size(),i=0;
+            char c;
+            while(i<lens && valid)
+            {
+                c=s[i];
+                if(c==')' || c=='}' || c==']')
+                {
+                    if(par.size()!=0) 
+                        {if((c==')' && par.top()=='(') || (c=='}' && par.top()=='{') || (c==']' && par.top()=='[')) par.pop();
+                        else valid=0;}
+                    else valid=0;
+                }
+                else
+                    par.push(c);
+                i++;
+            }
+            if(valid && par.size()==0)   return true;
+            return false;
+        }
+    };
