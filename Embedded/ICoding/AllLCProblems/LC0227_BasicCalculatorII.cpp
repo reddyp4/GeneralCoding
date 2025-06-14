2763345b1d4f89f8bde01aeea7dc840077fba8e4
reddyp4 <bhageerath.reddy@gmail.com>
2025-06-14 15:25:13 -0700

    update to mycalculator

--- Embedded/ICoding/AllLCProblems/LC0227_BasicCalculatorII.cpp
+++ Embedded/ICoding/AllLCProblems/LC0227_BasicCalculatorII.cpp
@@ -53,70 +53,52 @@ for every character
 */
 
 class Solution {
-private:
-    stack<int> myStk;
 public:
-    int getNumber(string s, int* loc, bool print,int len1)
-    {
-        int val=0;
-        while(s[*loc]==' ')    (*loc)++;
-        while(((*loc)<len1) && !(s[*loc]=='+' || s[*loc]=='-' || s[*loc]=='*' || s[*loc]=='/' || s[*loc]==' '))
-        {
-            val=val*10+(int)(s[*loc]-'0');
-            if(print)   cout<<"s[*loc]"<<s[*loc]<<",val:"<<val<<endl;
-            (*loc)++;
-        }
-        return val;
-    }
-
     int calculate(string s) {
-        int len1=s.size(),i=0,ans=0,val=0,oper1=0,oper2=0;
         char operation;
-        bool print=0;
-        if(print)   cout<<"len1:"<<len1<<endl;
-        while(i<len1)
+        int current=0,result=0;
+        stack<int> myStk;
+        for(int i=0;i<s.length();i++)
         {
-            if (s[i]==' ')    i++;    //space
-            else if(s[i]=='+')
+            if(s[i]==' ');
+            else if(isdigit(s[i]))
             {
-                myStk.push(1);i++;
-            }
-            else if(s[i]=='-')
-            {
-                myStk.push(-1);i++;
+                current=current*10+(s[i]-'0');
             }
             else if(s[i]=='*' || s[i]=='/')
             {
+                if(operation=='-') myStk.push(-current);
+                else  myStk.push(current);
                 operation=s[i];
-                //Operand1
-                oper1=myStk.top();
-                myStk.pop();
-                //Operand2
-                i++;
-                oper2=getNumber(s,&i,print,len1);
-                //higher priority operation, * or /
-                if(operation=='*')  val=oper1*oper2;
-                else if(operation=='/')  val=oper1/oper2;
-                if(print)   cout<<"oper1:"<<oper1<<",oper2:"<<oper2<<",val:"<<val<<endl;
-                myStk.push(val);
+                current=0;
             }
-            else
+            else if(s[i]=='+' || s[i]=='-')
             {
-                //number
-                val=getNumber(s,&i,print,len1);
-                if(!myStk.empty())  {val*=myStk.top();myStk.pop();}
-                if(print)   cout<<"val:"<<val<<endl;
-                myStk.push(val);
+                operation=s[i];
+                myStk.push(current);
+                current=0;
             }
         }
-        //pop stack
-        while(!myStk.empty())   
+        if(operation=='*')
         {
-            if(print)   cout<<"stk.top:"<<myStk.top()<<endl;
-            ans+=myStk.top();
+            int val=myStk.top()*current;
+            myStk.pop();
+            myStk.push(val);
+        }
+        else if(operation=='/')
+        {
+            int val=myStk.top()/current;
+            myStk.pop();
+            myStk.push(val);
+        }
+        else
+            myStk.push(current);
+        while(!myStk.empty())
+        {
+            //cout<<"stk.top:"<<myStk.top()<<endl;
+            result+=myStk.top();
             myStk.pop();
         }
-        if(print)   cout<<"ans:"<<ans<<endl;
-        return ans;
+        return result;
     }
 };
