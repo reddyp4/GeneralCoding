b8fc74f913371badae55c480661c91aa0dc8dfef
reddyp4 <bhageerath.reddy@gmail.com>
2025-06-27 18:46:44 -0700

    preorder w/o stack

---
+++ Embedded/ICoding/AllLCProblems/Stack/LC00144_BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,101 @@
+/*
+144. Binary Tree Preorder Traversal
+Solved
+Easy
+Topics
+conpanies icon
+Companies
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,2,3]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [1,2,4,5,6,7,3,8,9]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> vec;
+    map<TreeNode*,bool> seen;
+    void preorder(TreeNode* root)
+    {
+        vec.push_back(root->val);
+        if(root->left && !seen[root->left]) {
+            preorder(root->left);seen[root->left]=true;
+        }
+        if(root->right && !seen[root->right]) {
+            preorder(root->right);seen[root->right]=true;
+        }
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root==NULL)  return vec;
+        if(root->left==NULL && root->right==NULL) {
+            vec.push_back(root->val);return vec;    }
+        preorder(root);
+        return vec;
+    }
+};
+
+/*
+Runtime
+1
+ms
+Beats
+6.14%
+Analyze Complexity
+Memory
+11.52
+MB
+Beats
+8.98%
+*/
