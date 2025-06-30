3a62364f83cf9514ad77c9f6a8247a9e4b44cf69
reddyp4 <bhageerath.reddy@gmail.com>
2025-06-29 17:55:48 -0700

    iorder stack

--- Embedded/ICoding/AllLCProblems/Stack/LC00144_BinaryTreePreorderTraversal.cpp
+++ Embedded/ICoding/AllLCProblems/Stack/LC00144_BinaryTreePreorderTraversal.cpp
@@ -63,25 +63,94 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/*
+Method1: Recursively visit left node, until no child
+> visit node
+> visit right node
+Stack: 
+ > Node: push right, push node, visit left
+
+Method2: Stack
+> set root as seen, push right, root, left into stack
+> pop, 
+ > if no left, add to list
+> 
+*/ 
 class Solution {
 public:
     vector<int> vec;
-    map<TreeNode*,bool> seen;
-    void preorder(TreeNode* root)
+    map<TreeNode*,int> seen;
+    void inorder(TreeNode* root)
     {
+        seen[root]=true;
+        if(root->left && !seen[root->left])  inorder(root->left);
         vec.push_back(root->val);
-        if(root->left && !seen[root->left]) {
-            preorder(root->left);seen[root->left]=true;
-        }
-        if(root->right && !seen[root->right]) {
-            preorder(root->right);seen[root->right]=true;
-        }
+        if(root->right && !seen[root->right]) inorder(root->right);
     }
-    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> inorderTraversal(TreeNode* root) {
+        bool print=false;
+        /* Recursive solution */
+        /*
+        cout<<"Recursive method"<<endl;
         if(root==NULL)  return vec;
-        if(root->left==NULL && root->right==NULL) {
-            vec.push_back(root->val);return vec;    }
-        preorder(root);
+        inorder(root);
+        return vec;
+        */
+        /* Stack soluation */
+        TreeNode *node;
+        stack<TreeNode*> myStk;
+        if(root==NULL)  return vec;
+        if(root->left==NULL && root->right==NULL)   {
+            vec.push_back(root->val);
+            return vec; }
+        /*if(root->right!=NULL)   {
+            myStk.push(root->right);seen[root->right]=true; 
+            cout<<"Push1:"<<(root->right)->val<<endl;    }*/
+        myStk.push(root);seen[root]=true;
+        if(print)    cout<<"Push2:"<<root->val<<endl;
+        /*if(root->left!=NULL)   {
+            myStk.push(root->left);seen[root->left]=true;
+            cout<<"Push3:"<<(root->left)->val<<endl;   }*/
+        while(!myStk.empty())
+        {
+            //pop, if left -> visit recursively=push-right,node,left 
+            node=myStk.top();if(print)    cout<<"Current:"<<node->val<<endl;
+            if(node->left!=NULL && !seen[node->left]) {
+                /*if(node->right && !seen[node->right]) {
+                node=myStk.top();myStk.pop();
+                myStk.push(node);
+                cout<<"Push7:"<<node->val<<endl;    }
+                if(!seen[node]) {
+                    myStk.push(node);seen[node]=true;
+                    cout<<"Push5:"<<node->val<<endl;    }*/
+                myStk.push(node->left);seen[node->left]=true;
+                if(print)    cout<<"Push6:"<<(node->left)->val<<endl;
+            }
+            else if(node->right==NULL)    {
+                vec.push_back(node->val);myStk.pop();
+                if(print)    cout<<"vec:"<<vec[vec.size()-1]<<endl;
+                if(!myStk.empty() && print)   cout<<"new top:"<<(myStk.top())->val<<endl;
+            }
+            else if(!seen[node->right])   {
+                //left=NULL or seen, right is present
+                vec.push_back(node->val);myStk.pop();
+                if(print)    cout<<"vec:"<<vec[vec.size()-1]<<endl;
+                myStk.push(node->right);seen[node->right]=true;
+                if(print)    cout<<"Push4:"<<(node->right)->val<<endl;
+            }
+        }
+        if(print)    cout<<endl;
         return vec;
     }
 };
@@ -98,4 +167,19 @@ Memory
 MB
 Beats
 8.98%
+*/
+
+/*
+Stack solution
+Runtime
+0
+ms
+Beats
+100.00%
+Analyze Complexity
+Memory
+11.07
+MB
+Beats
+
 */
