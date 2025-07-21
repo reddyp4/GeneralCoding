358a9277652a46cd521aefca12bcc7ef8ba777b6
reddyp4 <bhageerath.reddy@gmail.com>
2025-07-20 22:10:51 -0700

    sub-array

--- Embedded/ICoding/AllLCProblems/TwoPointer/LC000560_SubarraySumEqualsK.cpp
+++ Embedded/ICoding/AllLCProblems/TwoPointer/LC000560_SubarraySumEqualsK.cpp
@@ -37,12 +37,15 @@ Bruteforce: for every i, move j until sum is achieved
     Optimize? 
         ? Maintain sum upto index i? sumj-sumi = sum of all elements between i and j
             At index, check [sum-to-end]-index is equal to k, if yes we have an array
+            Check for every combination
         ? Sort? May not help, since numbers are random
+HashMap Solution Using Cumulative Sum = sum = sum of all elements upto and including nums[i]
+    if(sum[i]==0)
 */
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int i,j,len1=nums.size(),sum=0,res=0;
+        /*int i,j,len1=nums.size(),sum=0,res=0;
         for(i=0;i<len1;i++) {
             sum=nums[i];
             if(sum==k)  res++;
@@ -53,11 +56,27 @@ public:
                 }
             }
         }
+        return res;*/
+        /* Hashmap solution */
+        unordered_map<int,int> myMap;
+        int i,j,res=0,len1=nums.size(),sum=0;
+        /* loop, get sum, update sum[k], check if sum-k=0, & key exists update count */
+        myMap[0]=1;
+        for(i=0;i<len1;i++) {
+            sum+=nums[i];
+            if(myMap.count(sum-k)>0)  {
+                //Already exists
+                res+=myMap[sum-k];
+                //cout<<nums[i]<<":"<<res<<endl;
+            }
+            if(myMap.count(sum)>0)  myMap[sum]++;
+            else    myMap[sum]=1;
+        }
         return res;
     }
 };
 
-/*
+/* i,j solution
 Runtime
 1411ms
 Beats22.65%
@@ -65,4 +84,13 @@ Analyze Complexity
 Memory
 34.30MB
 Beats93.79%
+
+Runtime
+39ms
+Beats78.18%
+Analyze Complexity
+Memory
+45.44MB
+Beats57.15%
+
 */
