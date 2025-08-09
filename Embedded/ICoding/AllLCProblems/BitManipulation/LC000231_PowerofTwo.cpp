b7c8788548f488917cc8df2f01698bcad3fadfc6
reddyp4 <bhageerath.reddy@gmail.com>
2025-08-09 10:25:31 -0700

    pow 2

---
+++ Embedded/ICoding/AllLCProblems/BitManipulation/LC000231_PowerofTwo.cpp
@@ -0,0 +1,57 @@
+/*
+231. Power of Two
+Solved
+Easy
+Topics
+conpanies icon
+Companies
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: n = 3
+Output: false
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1
+
+*/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)    return false;
+        if(n==-2147483648)  return false;
+        if((n&(n-1))==0)    return true;
+        return false;
+    }
+};
+
+/*
+Runtime
+0
+ms
+Beats
+100.00%
+Analyze Complexity
+Memory
+7.90
+MB
+Beats
+17.71%
+*/
